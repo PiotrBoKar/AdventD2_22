b04ff5100a26dd5c91f982c023a2aee7295b51fd
piotr <piotr@DESKTOP-8AS28RG>
2022-12-04 22:31:19 +0100

    Dodaj pliki projektów.

---
+++ AdventD2_22/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace AdventD2_22
+{
+    public class dayTwo22
+    {
+        public static void Main()
+        {
+            int result = 0;
+            string res;
+            List<string[]> input = ReadFile.getList("22D2");
+            for (int i = 0; i < input.Count; i++)
+            {
+                res = Result.getResult(String.Join("", input[i]));
+                result = result + int.Parse(res);
+            }
+            Console.WriteLine(result);
+            res = "";
+            result = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                res = Result.getCalculatedResult(String.Join("", input[i]));
+                result = result + int.Parse(res);
+            }
+            Console.WriteLine(result);
+
+        } 
+
+
+    }
+}
---
+++ AdventD2_22/Result.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventD2_22
+{
+    internal class Result
+    {
+        public static string getResult(string dName)
+        {
+            int result = 0;
+            string resultS="";
+            if (dName[0].Equals('A') & dName[1].Equals('Y') | dName[0].Equals('B') & dName[1].Equals('Z') | dName[0].Equals('C') & dName[1].Equals('X'))
+            {
+                result = result + 6;
+            }
+            else if (dName[0].Equals('A') & dName[1].Equals('X') | dName[0].Equals('B') & dName[1].Equals('Y') | dName[0].Equals('C') & dName[1].Equals('Z'))
+            {
+                result = result + 3;
+            }
+
+            if (dName[1].Equals('X'))
+            {
+                result = result + 1;
+            }
+            else if (dName[1].Equals('Y'))
+            {
+                result = result + 2;
+            }
+            else if (dName[1].Equals('Z'))
+            {
+                result = result + 3;
+            }
+
+            resultS = result.ToString();
+            return resultS;
+        }
+
+        public static string getCalculatedResult(string dName)
+        {
+            int result = 0;
+            string resultS = "";
+            if (dName[0].Equals('A') & dName[1].Equals('Y') | dName[0].Equals('B') & dName[1].Equals('X') | dName[0].Equals('C') & dName[1].Equals('Z'))
+            {
+                result = result + 1;
+            }
+            else if (dName[0].Equals('A') & dName[1].Equals('Z') | dName[0].Equals('B') & dName[1].Equals('Y') | dName[0].Equals('C') & dName[1].Equals('X'))
+            {
+                result = result + 2;
+            }
+            else if (dName[0].Equals('A') & dName[1].Equals('X') | dName[0].Equals('B') & dName[1].Equals('Z') | dName[0].Equals('C') & dName[1].Equals('Y'))
+            {
+                result = result + 3;
+            }
+
+            if (dName[1].Equals('X'))
+            {
+                result = result + 0;
+            }
+            else if (dName[1].Equals('Y'))
+            {
+                result = result + 3;
+            }
+            else if (dName[1].Equals('Z'))
+            {
+                result = result + 6;
+            }
+
+            resultS = result.ToString();
+            return resultS;
+        }
+    }
+}
+//rock=1
+//paper=2
+//scissors=3
+//loose = 0
+//draw = 3
+//win = 6
